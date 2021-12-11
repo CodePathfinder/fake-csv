616127dc67ac570b1ab145e1e172f7f28e66d8b5
Gennadiy Molchanov <gmolchanov@gmail.com>
2021-12-11 02:37:02 +0200

    if statement for db

--- planekstz/settings.py
+++ planekstz/settings.py
@@ -83,27 +83,27 @@ WSGI_APPLICATION = 'planekstz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "os.environ.get('DB_NAME')",
-        'USER': "os.environ.get('DB_USERNAME')",
-        'PASSWORD': "os.environ.get('DB_PASSWORD')",
-        'HOST': "os.environ.get('DB_HOST')",
-        'PORT': "os.environ.get('DB_PORT')"
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': "os.environ.get('DB_NAME')",
+            'USER': "os.environ.get('DB_USERNAME')",
+            'PASSWORD': "os.environ.get('DB_PASSWORD')",
+            'HOST': "os.environ.get('DB_HOST')",
+            'PORT': "os.environ.get('DB_PORT')"
+        }
     }
-}
+    # https://devcenter.heroku.com/articles/python-concurrency-and-database-connections
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-# https://devcenter.heroku.com/articles/python-concurrency-and-database-connections
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 AUTH_USER_MODEL = "schemas.User"
