63a673df93efecf7fc77e67b5ee952aa4fa8d80e
Gennadiy Molchanov <gmolchanov@gmail.com>
2021-12-14 18:07:26 +0200

    js files added

---
+++ schemas/static/schemas/datasets.js
@@ -0,0 +1,51 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    // call status_check
+    status_check();
+});
+
+let timerId = 0;
+
+// check pending task data status every 3 seconds
+function status_check() {
+    timerId = setInterval(() => {
+        getPendingDataStatus()
+    }, 3000);
+};
+
+// request status of pending data
+function getPendingDataStatus() {
+    let counter = 0
+    // console.log(`TIMER_ID: ${timerId}`)
+    
+    // loop for all link elements waiting for href attribute 
+    document.querySelectorAll('.pending-url').forEach(element => {
+        const href = element.getAttribute('href');
+        if (href === '') {
+            counter++
+            // get monitor_task_key as link element id
+            const id = element.getAttribute('id');
+            // send get request to server
+            fetch(`/status_check/${id}`)
+            .then(response => response.json())
+            .then(result => {
+                // console.log(result);
+                
+                if (result['url']) {
+                    // change elements properties
+                    element.setAttribute('href', result['url']);
+                    element.setAttribute('class', 'visible');
+                    const bttn = element.parentElement.previousElementSibling.querySelector('button');
+                    bttn.setAttribute('class', 'btn btn-success btn-sm');
+                    bttn.innerText = 'Ready';
+                }
+            });
+        }; 
+    });
+    if (counter === 0) {
+        // change setInterval function
+        clearInterval(timerId);
+        // console.log('STOP SETINTERVAL');
+    }
+}
---
+++ schemas/static/schemas/schema.js
@@ -0,0 +1,83 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    // clean type field after reload on relevant page   
+    document.querySelector('#id_data_type').value = "";
+    // add first colomn on relevant page
+    addColomn();
+    // add further colomns upon pless 'Add Colomn' button 
+    document.querySelector('#add-colomn').onclick = function() {
+        addColomn();
+    };
+});
+
+let counter = 0;
+
+// Add new blank colomn to colomn container
+function addColomn() {
+    
+    // get colomn form
+    const newcol = document.querySelector('#blank-colomn-form').innerHTML;
+
+    // wrap colomn form into div with mx-2
+    const element = document.createElement('div');
+    element.className = 'mx-2';
+    element.innerHTML = newcol;
+
+    // amend "id" and "name" properties for each input element
+    counter++;
+   
+    const col = element.querySelector('#id_colomn_name');
+    col.id = `id${counter}_colomn_name`;
+    col.name = `colomn_name_${counter}`;
+
+    const type = element.querySelector('#id_data_type');
+    type.id = `id${counter}_data_type`;
+    type.name = `data_type_${counter}`;
+
+    const from = element.querySelector('#id_range_from');
+    from.id = `id${counter}_range_from`;
+    from.name = `range_from_${counter}`;
+    from.parentElement.style.visibility = 'hidden';    
+
+    const to = element.querySelector('#id_range_to');
+    to.id = `id${counter}_range_to`;
+    to.name = `range_to_${counter}`;
+    to.parentElement.style.visibility = 'hidden';
+
+    const order = element.querySelector('#id_order_num');
+    order.id = `id${counter}_order_num`;
+    order.name = `order_num_${counter}`;
+
+    // add class 'col-delete' attributes to delete element
+    const del_col = element.querySelector('p');
+    del_col.setAttribute('class', `col-delete text-danger`);
+
+    // place new element to DOM
+    document.querySelector('#col-container').append(element);
+}
+
+// Remove colomn selected for deletion
+document.addEventListener('click', event => {
+    const element = event.target;
+    if (element.className === 'col-delete text-danger') {
+        element.parentElement.parentElement.remove();
+    }    
+});
+
+// Visiolise range colomns when 'integer' or 'text' type is selected
+document.addEventListener('change', event => {
+    const element = event.target;
+    if (element.className === 'form-select mb-3') {
+        // reach range elements of relevant colomn through grandparent element 'row'
+        const range_els = element.parentElement.parentElement.querySelectorAll('.range');
+        range_els.forEach(range=> {
+            // if 'integer' or 'text' option is selected, change visibility
+            if (element.value === '8' || element.value === '7') {
+                range.style.visibility = 'visible';
+            } else {
+                range.style.visibility = 'hidden';
+            }
+        });
+    };   
+});
